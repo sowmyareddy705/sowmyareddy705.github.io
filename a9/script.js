13ed9cc39cf9c9974ee905ea90e829c2eb97b9fe
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-05-14 21:06:47 +0530

    UPDATE

---
+++ a9/script.js
@@ -0,0 +1,16 @@
+const fruits = ["Apple", "Orange", "Banana", "Berries", "Pineapple", "Grapes"];
+
+function searchFruit() {
+  const input = document.getElementById("fruit-input").value.trim().toLowerCase();
+  const result = fruits.filter(fruit => fruit.toLowerCase() === input);
+
+  const resultBox = document.getElementById("result-box");
+
+  if (result.length > 0) {
+    resultBox.textContent = "Result: " + result[0];
+    resultBox.style.color = "green";
+  } else {
+    resultBox.textContent = "Result: Not Available";
+    resultBox.style.color = "red";
+  }
+}
