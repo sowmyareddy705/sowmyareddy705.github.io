533ffde17d3c38f705f154bfad1a6a62b3d13795
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-05-16 19:29:29 +0530

    update

---
+++ a12/index.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 1, name: "Product 1", price: 34 },
+  { id: 1, name: "Product 2", price: 56 },
+  { id: 1, name: "Product 3", price: 40 },
+];
+
+products.forEach((product) => console.log(product.name));
