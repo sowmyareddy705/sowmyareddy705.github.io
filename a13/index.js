8c1f064f38aa12483b480aa5f01d56a50acebd0f
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-05-16 19:44:11 +0530

    update

--- a13/index.js
+++ a13/index.js
@@ -1,10 +1,10 @@
 const products = [
   { id: 1, name: "Product 1", price: 34 },
   { id: 1, name: "Product 2", price: 56 },
-  { id: 1, name: "Product 3", price: 40 }
+  { id: 1, name: "Product 3", price: 40 },
 ];
 
-for (let i = 0; i < products.length; i++) {
-  products[i].price = products[i].price + 5;
-  console.log(products[i].name + ": " + products[i].price);
-}
+products.map(product => {
+  product.price += 5;
+  console.log(product.name + ": " + product.price);
+});
